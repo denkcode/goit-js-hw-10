8aa0d265b414950a8aed35947567b2cd16fe2781
fqsocite <denispenzinn@gmail.com>
2026-01-08 19:59:42 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,46 @@
 import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
+import { resolve } from 'path';
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/goit-js-hw-10/',
-
-  define: {
-    [command === 'serve' ? 'global' : '_global']: {},
-  },
-
-  build: {
-    sourcemap: true,
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        index: './index.html',
-        timer: './1-timer.html',
-        snackbar: './2-snackbar.html',
-      },
-      output: {
-        // Всі JS у корінь dist, щоб шляхи у HTML працювали на GitHub Pages
-        entryFileNames: '[name].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        manualChunks(id) {
-          if (id.includes('node_modules')) return 'vendor';
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    base: '/goit-js-hw-09/',
+    build: {
+      sourcemap: true,
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          gallery: resolve(__dirname, 'src/1-timer.html'),
+          form: resolve(__dirname, 'src/2-snackbar.html'),
+        },
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: 'assets/[name]-[hash][extname]',
         },
       },
     },
-  },
-
-  plugins: [
-    FullReload(['./*.html', './src/*.html']),
-    SortCss({ sort: 'mobile-first' }),
-  ],
-}));
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/*.html']),
+      SortCss({ sort: 'mobile-first' }),
+    ],
+  };
+});
